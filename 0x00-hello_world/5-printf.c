00683181322e3c58d1322d24e081e157bbcd7ea0
mohamedeshahat <mohamedelshahat373@gmail.com>
2023-03-16 21:48:30 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 /**
- * hllo
+ * main - entry point
+ *
+ * description: A c program that prints with printf function
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf ("with proper grammar, but the outcome is a piece of art,/n");
+	printf("with proper grammar, but the outcome is a piece of art,/n");
 	return (0);
 }
