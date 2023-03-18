1dfc35993f4d5ec1228c43e9a8d79cd4be5bd258
mohamedeshahat <mohamedelshahat373@gmail.com>
2023-03-17 18:43:18 -0700

    8

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * description: this is a c program that prints alphabets
+ *
+ * Return: alaeys 0 (success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'z' ; i >= 'a' ; i--)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
