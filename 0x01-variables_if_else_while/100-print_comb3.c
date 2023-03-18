7e7da4ec274af2358906292b2f0f39cd5877e7f6
mohamedeshahat <mohamedelshahat373@gmail.com>
2023-03-17 19:18:49 -0700

    11

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * description: this is a c program that prints alphabets and numbers
+ *
+ * Return: alaeys 0 (success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (j = 1; j < 10 ; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			if (i + j != 17)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
