22b7567d150d9d7bf1e48ebe12ebd12f3a7985cb
mohamedeshahat <mohamedelshahat373@gmail.com>
2023-03-21 14:34:21 -0700

    8

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/*
+ * print_last_digit - ti print the last number
+ * @n: jist paramter
+ * return: last vlur
+ */
+int print_last_digit(int n)
+{
+	int last;
+
+	last = n % 10;
+	if (last < 0)
+	{
+		last = last * -1;
+	}
+	_putchar(last + '0');
+	return (last);
+}
