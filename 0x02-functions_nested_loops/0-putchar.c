80818a018b156cf47f88219a13634b9c8ce1b01e
mohamedeshahat <mohamedelshahat373@gmail.com>
2023-03-21 07:19:45 -0700

    1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include "main.h"
 /**
  * main - Entry point
  *
@@ -8,11 +8,14 @@
  */
 int main(void)
 {
-	int x[] = {'_','p','u','t','c','h','a','r'};
-	
-	for (int i = 0; i > 8; i++)
-	{
-		putchar(x[i]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
