2cc6b0a03f1631c410b5c38b24dc730a3b9cdcf8
mohamedeshahat <mohamedelshahat373@gmail.com>
2023-03-21 06:27:48 -0700

    1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * description: this is a c program that prints alphabets and numbers
+ *
+ * Return: alaeys 0 (success)
+ */
+int main(void)
+{
+	int x[] = {'_','p','u','t','c','h','a','r'};
+	
+	for (int i = 0; i > 8; i++)
+	{
+		putchar(x[i]);
+	}
+	return (0);
+}
