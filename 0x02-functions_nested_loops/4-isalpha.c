42bab6e28f0afdc37ed88a978d4a98b18c38f23e
mohamedeshahat <mohamedelshahat373@gmail.com>
2023-03-21 12:48:46 -0700

    5

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,15 +1,14 @@
 #include "main.h"
 /**
- * _islower - lowercase checking
- * @c: is paramter to store charater
- * Return: if the charater is lowercase return 1 anything else retun 0
+ * _isalpha - checking for alohabet
+ * @c: is a paramter
+ * Return: if it is a character return 1 anything else return 0
  */
 int _isalpha(int c)
 {
-        if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
-        {
-                return (1);
-        }
-        return (0);
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+	return (0);
 }
-			
