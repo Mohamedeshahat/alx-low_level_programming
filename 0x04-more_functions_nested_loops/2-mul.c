4ec3d112921cd214d0a307a7dd6824b8a52efb16
mohamedeshahat <mohamedelshahat373@gmail.com>
2023-03-24 05:11:36 -0700

    3

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include"main.h"
+/**
+ * mul - program to multiplr two numbers
+ *
+ * @a: is a paramter
+ *
+ * @b: is a paramter
+ *
+ * Return: return a and b
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
