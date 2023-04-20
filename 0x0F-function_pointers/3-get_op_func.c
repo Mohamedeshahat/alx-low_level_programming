11ebef2336df3a6e294d4b04ee351da1d8b0f23c
mohamedeshahat <mohamedelshahat373@gmail.com>
2023-04-19 23:37:38 -0700

    4

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,7 +1,7 @@
 #include"3-calc.h"
 #include <stddef.h>
 /**
- * get_op_fun - returns pointer to right op function
+ * get_op_func - returns pointer to right op function
  * @s: string operator
  *
  * Return: the right function
@@ -16,11 +16,11 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	int i= 0;
+	int i = 0;
 
 	while (i < 5)
 	{
-		if ( s && s[0] == ops[i].op[0] && !s[1])
+		if (s && s[0] == ops[i].op[0] && !s[1])
 			return (ops[i].f);
 		i++;
 	}
