583440be314e9487eb34e5fc926a40aafba174ec
mohamedeshahat <mohamedelshahat373@gmail.com>
2023-04-19 19:28:18 -0700

    4

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,10 +14,9 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (array && size && cmp)
 		while (i < size)
 		{
-			if (cmp(array[i])
-			{
-			return (i);
-			}
-		i++;
+			if (cmp(array[i]))
+				return (i);
+			i++;
 		}
+	return (-1);
 }
