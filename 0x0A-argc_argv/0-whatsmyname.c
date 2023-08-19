d757083c6c6901bd235ea3c8e1214e244c71b751
mohamed <mohamedelshahat373@gmail.com>
2023-08-19 10:01:56 -0700

    1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - the entry point
+ * @argc: the number of  arguments
+ * @argv: the array of pointer
+ * Return: void
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
