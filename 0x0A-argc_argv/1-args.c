9569016032298e1411cc95509c1241515e252a1c
mohamed <mohamedelshahat373@gmail.com>
2023-08-20 10:04:44 -0700

    1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ * @argc: number  of  aguments
+ * @argv: arguments vales
+ * Return: void
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc);
+	return (0);
+}
