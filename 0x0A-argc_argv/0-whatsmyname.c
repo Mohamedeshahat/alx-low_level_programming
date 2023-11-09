4a4ff2be5c790e6e48ce87976f322b4bcb1c5cf5
Mohamedeshahat <mohamedelshahat373@gmail.com>
2023-11-09 05:43:51 -0800

    1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - this function to print the program's name
+ * @argc: paramter
+ * @argv: paramter
+ * Return: alywas o
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
