c23bbd3fa5802c70a1e4ff222590e43d346f9c5b
Mohamedeshahat <mohamedelshahat373@gmail.com>
2023-12-25 12:14:41 -0800

    5

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include"main.h"
+/**
+ * print_rev - print in reverse
+ * @s: parametre
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i = 1;
+
+	while (*s != '\0')
+	{
+		s++;
+		i++;
+	}
+	while (i != 0)
+	{
+		_putchar(*s);
+		s--;
+		i--;
+	}
+	_putchar('\n');
+}
+
