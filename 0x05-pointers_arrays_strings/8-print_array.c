ceae63d259846cb8b11b98579b077f10aeaa1f89
Mohamedeshahat <mohamedelshahat373@gmail.com>
2024-04-19 22:06:44 -0700

    3

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,18 +1,18 @@
 #include"main.h"
 #include<stdio.h>
 /**
- * print _array - funcrion for printing array
+ * print_array - funcrion for printing array
  * @a: parameter
  * @n: parameter
  * Return: zero
  */
 void print_array(int *a, int n)
 {
-	int i;
+	unsigned int i;
 
-	for (i =0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
-	printf("%d, ", *(a + i));
+		printf("%d, ", *(a + i));
 	}
-	printf("\n");
-}	
+printf("\n");
+}
