255fddc3c53c908a61d835f12e6f3641c916e22d
mohamed <mohamedelshahat373@gmail.com>
2024-09-03 06:51:38 -0700

    10

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n <= 9)
+	{
+		putchar(n + 48);
+		if (n < 9)
+		{
+			putchar(';');
+			putchar(' ');
+		}
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
