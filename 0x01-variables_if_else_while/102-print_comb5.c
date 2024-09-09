dad90314d656c43e6a304ef7ad902313205028b1
mohamed <mohamedelshahat373@gmail.com>
2024-09-08 21:53:35 -0700

    13

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,17 +6,17 @@
  */
 int main(void)
 {
-        int n1 = 0;
-        int n2 = 0;
-        int n3 = 0;
+	int n1 = 0;
+	int n2 = 0;
+	int n3 = 0;
 	int n4 = 1;
 
-        while (n1 <= 9)
-        {
-                while (n2 <= 9)
-                {
-                        while (n3 <= 9)
-                        {
+	while (n1 <= 9)
+	{
+		while (n2 <= 9)
+		{
+			while (n3 <= 9)
+			{
 				while (n4 <= 9)
 				{
 					if ((n4 > n3 && n3 > n2 && n2 > n1) && (n1 != n2 && n2 != n3 && n3 != n4))
@@ -26,22 +26,19 @@ int main(void)
 						putchar(' ');
 						putchar(n3 + 48);
 						putchar(n4 + 48);
-						if (n1 + n2 + n3 != 24)
-						{
-							putchar(',');
-							putchar(' ');
-						}
+						putchar(',');
+						putchar(' ');
 					}
 					n4++;
 				}
 				n4 = 0;
-                                n3++;
-                        }
-                        n3 = 0;
-                        n2++;
-                }
-                n2 = 0;
-                n1++;
+				n3++;
+			}
+			n3 = 0;
+			n2++;
+		}
+		n2 = 0;
+		n1++;
 	}
 	putchar ('\n');
 	return (0);
